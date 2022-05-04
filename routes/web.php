4b00385049578657f4d3daa89fddd783afe311e0
Yuri Marcos Spirandeli Filho <spirandelidev@gmail.com>
2022-05-04 13:23:21 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('/contato', 'ContatoController@contato');
+ 
